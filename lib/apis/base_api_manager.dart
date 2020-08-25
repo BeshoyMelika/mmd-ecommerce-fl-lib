bd572c180fae6096fa636a2d28f804574a280df2
MinaSamir <menasamercap@gmail.com>
2020-08-25 17:58:14 +0200

    enhanced client to be only one object

--- lib/apis/base_api_manager.dart
+++ lib/apis/base_api_manager.dart
@@ -2,15 +2,19 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 
 class BaseApiManager {
   static String _url;
+  static GraphQLClient _client;
 
   static void setUrl(baseUrl) {
     _url = baseUrl + "/graphql";
   }
 
   static GraphQLClient client() {
-    return GraphQLClient(
-      cache: InMemoryCache(),
-      link: HttpLink(uri: _url),
-    );
+    if (_client == null) {
+      _client = GraphQLClient(
+        cache: InMemoryCache(),
+        link: HttpLink(uri: _url),
+      );
+    }
+    return _client;
   }
 }
