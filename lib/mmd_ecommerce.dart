32513bd683f0481edcb5a142ad4fea1948010587
Beshoy Melika <beshoygamal310@gmail.com>
2020-08-25 16:48:25 +0200

    fixed error in export graphql_flutter

--- lib/mmd_ecommerce.dart
+++ lib/mmd_ecommerce.dart
@@ -1,8 +1,9 @@
 library mmd_ecommerce_fl_lib;
 
+export 'package:graphql_flutter/graphql_flutter.dart';
+
 export './mmd_ecommerce.dart';
 export './mmd_ecommerce_fl_lib.dart';
 export './graphql_api.dart';
 export './apis/auth_api_manager.dart';
 export './apis/general_api_manager.dart';
-export 'package:graphql/src/core/query_result.dart';
