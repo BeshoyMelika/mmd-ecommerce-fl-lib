1736cea0a9c2e030729ca8c26ae4d5f0a345b2a3
MinaSamir <menasamercap@gmail.com>
2020-09-03 23:35:49 +0200

    separated some classes

---
+++ example/lib/apis/auth_apis.dart
@@ -0,0 +1,33 @@
+import 'package:mmd_ecommerce_fl_lib/mmd_ecommerce.dart';
+
+callRegisterApi() {
+  AuthApiManager.registerApi("mmd10", "rovox40676@rika0525.com", "123456",
+          (UserRegister user) {
+        print("=========================================");
+        print("user email : " + user.user.email + "name :" + user.user.name);
+        print("Success register API");
+      }, (QueryResult x) {
+        print("error");
+      });
+}
+
+callForgetPasswordApi(String email) {
+  AuthApiManager.forgetPasswordApi(email, (bool success) {
+    print("=========================================");
+    print("Success forget api $success");
+  }, (QueryResult error) {
+    print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx");
+    print("Fail forget api");
+  });
+}
+
+callResetPasswordApi(String email, String newPass, String code) {
+  AuthApiManager.resetPasswordApi(email, newPass, code, (bool success) {
+    print("=========================================");
+    print("Success Reset pass api $success");
+  }, (QueryResult error) {
+    print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx");
+    print("Fail reset pass api");
+    print(error.data);
+  });
+}
